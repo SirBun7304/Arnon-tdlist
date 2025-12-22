e76bee445cedbc187a8fb174a30f5b1326746b36
Patthakon <sanit7304@gmail.com>
2025-12-22 10:11:07 +0700

    feat:init leader

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ARNON-TDLIST</title>
+</head>
+
+<body>
+    <h1>ARNON-TDLIST</h1>
+    <hr />
+    <h3>ปัฐถากรณ์ อู่อรุณ(บุญ)</h3>
+    <h3>Patthakon Auarun(Boon)</h3>
+    <hr />
+</body>
+
+</html>
