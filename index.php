a597c73134e6b05b1fa2ff6f7a240e377dd12dbe
Boon <sanit7304@gmail.com>
2025-12-23 19:02:59 +0700

    ประวัต

--- index.php
+++ index.php
@@ -8,7 +8,23 @@
 </head>
 
 <body>
-<h1>ควย บุน</h1>
+    <h1>ควย บุน</h1>
+    <hr />
+    <h3>ปัฐถากรณ์ อู่อรุณ(บุญ)</h3>
+    <h3>Patthakon Auarun(Boon)</h3>
+    <hr />
+    <h3>พัสกร วาปี(ดิว)</h3>
+    <h3>Passakron Wapee(Dew)</h3>
+    <p>test</p>
+    <p>tset2</p>
+    <p>tset3</p>
+    <p>tset4</p>
+    <p>tset5</p>
+    <p>tset6</p>
+    <p>tset7</p>
+    <p>tset8</p>
+    <p>tset9</p>
+    <p>tset10</p>
 </body>
 
 </html>
